573d294b15262e00cfd99c4a2e8961b58b71aae9
Igor Shumeyko <ishumeyko@cogniance.com>
2013-04-19 19:34:13 +0300

    0001. Some added files

---
+++ MeCodez/src/com/shumz/think/extwo/ExTwo10.java
@@ -0,0 +1,36 @@
+package com.shumz.think.extwo;
+
+public class ExTwo10 {
+
+	public static void main(String[] args) {
+
+		final int digit1 = 0xA45FA527;
+		final int digit2 = 0x8DC3B00F;
+
+		System.out.println("              +--------------------------------+");
+		System.out.println("Digit one is: |" + Integer.toBinaryString(digit1) + "|");
+		System.out.println("              +--------------------------------+");
+		System.out.println("Digit two is: |" + Integer.toBinaryString(digit2) + "|");
+		System.out.println("              +--------------------------------+");
+		
+		System.out.println("     AND (&)  |" + Integer.toBinaryString(digit1 & digit2) + "|");
+		System.out.println("              +--------------------------------+");
+		System.out.println("      OR (|)  |" + Integer.toBinaryString(digit1 | digit2) + "|");
+		System.out.println("              +--------------------------------+");
+		System.out.println("     XOR (^)  |00" + Integer.toBinaryString(digit1 ^ digit2) + "|");
+		System.out.println("              +--------------------------------+");
+		System.out.println("     NOT (~)  |0" + Integer.toBinaryString(~digit1) + "|");
+		System.out.println("              +--------------------------------+");
+		
+		
+		System.out.println();
+	
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+	
+	}
+
+}
