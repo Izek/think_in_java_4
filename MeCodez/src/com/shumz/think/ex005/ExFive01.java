ffd11ec416b84a75f9a0e16caf86415ad5a23fb1
Igor Shumeyko <ishumeyko@cogniance.com>
2013-05-14 19:53:33 +0300

    0007. Access Control ch. was started.

---
+++ MeCodez/src/com/shumz/think/ex005/ExFive01.java
@@ -0,0 +1,14 @@
+package com.shumz.think.ex005;
+
+import com.shumz.think.ex004.ExFour19;
+
+public class ExFive01 {
+
+	
+	public static void main(String[] args) {
+
+		ExFour19 obj_ex_four = new ExFour19();
+		obj_ex_four.stringImporter("qwerty", "asdfgh", "zxcvbn");
+	}
+
+}
---
+++ MeCodez/src/com/shumz/think/ex005/ExFive02.java
@@ -0,0 +1,14 @@
+package com.shumz.think.ex005;
+
+import net.mindview.simple.*;
+import java.util.Vector;
+
+public class ExFive02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Vector v = new Vector();
+		net.mindview.simple.Vector v2 = new net.mindview.simple.Vector();
+	}
+
+}
