cfa98259d5793849d8bf30f9f22dd7fa41ecfa98
Igor Shumeyko <ishumeyko@cogniance.com>
2013-05-15 12:47:21 +0300

    0009. Refactoring was performed.

--- MeCodez/src/com/shumz/think/ex001/ShowProperties.java
+++
@@ -1,9 +0,0 @@
-package com.shumz.think.ex001;
-//: object/ShowProperties.java
-public class ShowProperties {
-	public static void main(String[] args) {
-		System.getProperties().list(System.out);
-		System.out.println(System.getProperty("user.name"));
-		System.out.println(System.getProperty("java.library.path"));
-	}
-} // /:~
