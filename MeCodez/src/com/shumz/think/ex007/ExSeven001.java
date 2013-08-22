c43cbb3d07b2f3e145c19afa41969ca0cbdf6697
Igor Shumeyko <ishumeyko@cogniance.com>
2013-08-22 13:04:08 +0300

    0013. 'Polymorphism' chapter was started

---
+++ MeCodez/src/com/shumz/think/ex007/ExSeven001.java
@@ -0,0 +1,13 @@
+package com.shumz.think.ex007;
+
+public class ExSeven001 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
